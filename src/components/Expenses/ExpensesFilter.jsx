0a144096da3d09b46da32cb6379aeb921f4dd675
Nazgul <kubanychbekovan6@gmail.com>
2022-10-19 17:01:01 +0300

    commit

--- src/components/Expenses/ExpensesFilter.jsx
+++ src/components/Expenses/ExpensesFilter.jsx
@@ -7,10 +7,10 @@ const ExpensesFilter = ({ selectedYear, onChangeSelectedYear }) => {
 				<label>Filter by year</label>
 				<select value={selectedYear} onChange={onChangeSelectedYear}>
 					<option value='All'>Select All</option>
-					<option value='2025'>2025</option>
-					<option value='2024'>2024</option>
 					<option value='2023'>2023</option>
 					<option value='2022'>2022</option>
+					<option value='2021'>2021</option>
+					<option value='2020'>2020</option>
 				</select>
 			</div>
 		</div>
